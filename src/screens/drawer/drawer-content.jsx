c04fe34fc351ab2e0025a80c06a0ca0c67a84d69
lehuy16 <cwalklhuy@gmail.com>
2020-10-10 01:02:58 +0700

    Thêm trạng thái hoạt động, ko hoạt động

--- src/screens/drawer/drawer-content.jsx
+++ src/screens/drawer/drawer-content.jsx
@@ -14,26 +14,112 @@ import {
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 function DrawerContent(props) {
+
+  const [isAction, setIsAction] = React.useState(false);
+
+  const toggleAction = () => {
+    setIsAction(!isAction);
+  }
+
   return (
     <View style={{ flex: 1 }}>
       <DrawerContentScrollView {...props}>
         <View style={styles.drawerContent}>
           <View style={styles.userInfoSection}>
-            <View style={{flexDirection:'row', marginTop:15}}>
+            <View style={{ flexDirection: 'row', marginTop: 15 }}>
               <Avatar.Image
                 source={{
                   uri: 'https://scontent.fdad3-1.fna.fbcdn.net/v/t1.0-9/83012519_1497814183728497_1901903877645533184_o.jpg?_nc_cat=102&_nc_sid=09cbfe&_nc_ohc=l92aofIVAloAX99oBIy&_nc_ht=scontent.fdad3-1.fna&oh=ac2b60cb37775a47a9c2ccc98f38fd2d&oe=5FA585D7'
                 }}
                 size={50}
               />
-              <View style={{flexDirection:'column',marginLeft:15}}>
+              <View style={{ flexDirection: 'column', marginLeft: 15 }}>
                 <Title style={styles.title}>Lê Quang Huy</Title>
                 <Caption>@lehuy.fptu</Caption>
               </View>
             </View>
           </View>
+          <Drawer.Section style={styles.drawerSection}>
+            <DrawerItem
+              icon={({ color, size }) => (
+                <Icon
+                  name="home-outline"
+                  color={color}
+                  size={size}
+                />
+              )}
+              label="Trang chủ"
+              onPress={() => { }}
+            />
+            <DrawerItem
+              icon={({ color, size }) => (
+                <Icon
+                  name="car-estate"
+                  color={color}
+                  size={size}
+                />
+              )}
+              label="Đăng ký xe"
+              onPress={() => { }}
+            />
+            <DrawerItem
+              icon={({ color, size }) => (
+                <Icon
+                  name="license"
+                  color={color}
+                  size={size}
+                />
+              )}
+              label="Đăng ký lái xe"
+              onPress={() => { }}
+            />
+            <DrawerItem
+              icon={({ color, size }) => (
+                <Icon
+                  name="history"
+                  color={color}
+                  size={size}
+                />
+              )}
+              label="Lịch sử"
+              onPress={() => { }}
+            />
+            <DrawerItem
+              icon={({ color, size }) => (
+                <Icon
+                  name="file-document-edit-outline"
+                  color={color}
+                  size={size}
+                />
+              )}
+              label="Hợp đồng"
+              onPress={() => { }}
+            />
+            <DrawerItem
+              icon={({ color, size }) => (
+                <Icon
+                  name="account-check-outline"
+                  color={color}
+                  size={size}
+                />
+              )}
+              label="Tài khoản"
+              onPress={() => { }}
+            />
+          </Drawer.Section>
+          <Drawer.Section title="Tình trạng">
+            <TouchableRipple onPress={() => {toggleAction()}}>
+              <View style={styles.preference}>
+                <Text>Hoạt động</Text>
+                <View pointerEvents="none">
+                  <Switch value={isAction} />
+                </View>
+              </View>
+            </TouchableRipple>
+          </Drawer.Section>
         </View>
       </DrawerContentScrollView>
+
       <Drawer.Section style={styles.bottomDrawerSection}>
         <DrawerItem
           icon={({ color, size }) => (
