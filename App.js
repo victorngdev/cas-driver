4290fb792d238aba3b03557138e798a838bde264
Victor Nguyễn <hoangloi669@gmail.com>
2020-10-10 15:49:47 +0700

    merge into dev/huy-coding

--- App.js
+++ App.js
@@ -1,55 +1,45 @@
 import React from "react";
 
 import { AppLoading } from "expo";
-import {
-  useFonts,
-  Roboto_500Medium,
-  Roboto_900Black,
-  Roboto_700Bold,
-} from "@expo-google-fonts/roboto";
-import Roboto_Regular from './assets/fonts/Roboto-Regular.ttf';
+import { useFonts, Roboto_500Medium, Roboto_900Black, Roboto_700Bold } from "@expo-google-fonts/roboto";
+import Roboto_Regular from "./assets/fonts/Roboto-Regular.ttf";
 
-import {
-  Nunito_400Regular,
-  Nunito_800ExtraBold,
-} from "@expo-google-fonts/nunito";
-import {NavigationContainer} from '@react-navigation/native';
-import {createDrawerNavigator} from '@react-navigation/drawer';
-
+import { Nunito_400Regular, Nunito_800ExtraBold } from "@expo-google-fonts/nunito";
+import { NavigationContainer } from "@react-navigation/native";
+import { createDrawerNavigator } from "@react-navigation/drawer";
 
 import LoginScreen from "./src/screens/login/login-screen";
 
-import DrawerContent from './src/screens/drawer/drawer-content';
+import DrawerContent from "./src/screens/drawer/drawer-content";
 import HomeScreen from "./src/screens/home/home-screen";
 import AccountScreen from "./src/screens/account-info/account-info";
 
-
 const Drawer = createDrawerNavigator();
 
-export function App () {
-  return (
-    <NavigationContainer>
-      <Drawer.Navigator initialRouteName="Home" drawerContent={props => <DrawerContent {...props} />}>
-        <Drawer.Screen name="Home" component={HomeScreen}/>
-        <Drawer.Screen name="Login" component={LoginScreen}/>
-        <Drawer.Screen name="AccountInfo" component={AccountScreen}/>
-      </Drawer.Navigator>
-    </NavigationContainer>
-  )
+export function App() {
+    return (
+        <NavigationContainer>
+            <Drawer.Navigator initialRouteName="Home" drawerContent={props => <DrawerContent {...props} />}>
+                <Drawer.Screen name="Home" component={HomeScreen} />
+                <Drawer.Screen name="Login" component={LoginScreen} />
+                <Drawer.Screen name="AccountInfo" component={AccountScreen} />
+            </Drawer.Navigator>
+        </NavigationContainer>
+    );
 }
 
 export default () => {
-  let [fontsLoaded] = useFonts({
-    Roboto_500Medium,
-    Roboto_900Black,
-    Roboto_700Bold,
-    Nunito_400Regular,
-    Nunito_800ExtraBold,
-    Roboto_Regular
-  });
-  if (!fontsLoaded) {
-    return <AppLoading />;
-  }
-  // from the custom App we return the component we assigned to AppContainer.
-  return <App />;
+    let [fontsLoaded] = useFonts({
+        Roboto_500Medium,
+        Roboto_900Black,
+        Roboto_700Bold,
+        Nunito_400Regular,
+        Nunito_800ExtraBold,
+        Roboto_Regular
+    });
+    if (!fontsLoaded) {
+        return <AppLoading />;
+    }
+    // from the custom App we return the component we assigned to AppContainer.
+    return <App />;
 };
--- src/components/text-input-with-icon.component.jsx
+++ src/components/text-input-with-icon.component.jsx
@@ -1,43 +1,39 @@
 import React from "react";
 import { View, Image, StyleSheet, TextInput, Dimensions } from "react-native";
 import rem from "./constant.unit";
-const TextInputIcon = (props) => {
-  return (
-    <View style={styles.container}>
-      <View style={styles.bgr_image}>
-        <Image source={props.imgSrc} style={styles.image} />
-      </View>
-      <TextInput
-        style={styles.text_input}
-        placeholder={props.placeholder}
-        placeholderTextColor="#FFF"
-      />
-    </View>
-  );
+const TextInputIcon = props => {
+    return (
+        <View style={styles.container}>
+            <View style={styles.bgr_image}>
+                <Image source={props.imgSrc} style={styles.image} />
+            </View>
+            <TextInput style={styles.text_input} placeholder={props.placeholder} placeholderTextColor="#FFF" />
+        </View>
+    );
 };
 const styles = StyleSheet.create({
-  container: {
-    backgroundColor: "rgba(255,255,255,0.4)",
-    flexDirection: "row",
-    width: "80%",
-    borderRadius: 60,
-    marginBottom: 15,
-  },
-  bgr_image: {
-    backgroundColor: "#FFF",
-    padding: 13,
-    borderRadius: 70,
-  },
-  image: {
-    width: 16,
-    height: 16,
-  },
-  text_input: {
-    fontSize: 16,
-    fontFamily: "Roboto_500Medium",
-    color: "#FFF",
-    marginLeft: 7,
-    width: 15 * rem,
-  },
+    container: {
+        backgroundColor: "rgba(255,255,255,0.4)",
+        flexDirection: "row",
+        width: "80%",
+        borderRadius: 60,
+        marginBottom: 15
+    },
+    bgr_image: {
+        backgroundColor: "#FFF",
+        padding: 13,
+        borderRadius: 70
+    },
+    image: {
+        width: 16,
+        height: 16
+    },
+    text_input: {
+        fontSize: 16,
+        fontFamily: "Roboto_500Medium",
+        color: "#FFF",
+        marginLeft: 7,
+        width: 15 * rem
+    }
 });
 export default TextInputIcon;
