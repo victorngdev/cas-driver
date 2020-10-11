34629e87b11993a2c52fd2324ca21af16c1c92f2
lehuy16 <cwalklhuy@gmail.com>
2020-10-11 21:12:27 +0700

    no message

--- src/screens/home/home-screen.jsx
+++ src/screens/home/home-screen.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import BackgroundImage from '../../components/background-screen.component';
 import styles from './home-style';
 import { createStackNavigator } from '@react-navigation/stack';
@@ -17,10 +17,10 @@ function HomeScreen(props) {
           alignItems: 'center',
           paddingHorizontal: 20
         }}>
-          
+
           <Icon name='menu' size={30} color='#a2a2db' style={{
             width: 20
-          }} onPress={() => props.navigation.openDrawer()}/>
+          }} onPress={() => props.navigation.openDrawer()} />
 
           <Text style={{
             fontSize: 20,
@@ -28,12 +28,19 @@ function HomeScreen(props) {
             color: '#522289',
             marginLeft: 50
           }}>Trang chủ</Text>
-        {/* ko hieu */}
+          {/* ko hieu */}
         </View>
       </BackgroundImage>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "column"
+  }
+});
 
 export default HomeScreen;
 
