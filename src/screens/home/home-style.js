f326d4ac53f305f99a461b93f54aa5e46b4d2746
Phung <nguyenphungse@gmail.com>
2020-10-13 01:57:29 +0700

    Home Screen Update

--- src/screens/home/home-style.js
+++ src/screens/home/home-style.js
@@ -8,81 +8,77 @@ const styles = StyleSheet.create({
     flex: 1,
     flexDirection: "column",
   },
-  userInfoSection: {
-    paddingLeft: 20,
-  },
   header: {
+    flex: 1,
     flexDirection: 'row',
-    marginTop: 30
+    marginTop:20,
+
   },
-  title: {
-    fontSize: 16,
-    marginTop: 3,
-    fontWeight: 'bold',
+  childHeader: {
+    flexDirection: 'row',
+    flex: 1,
+    marginTop:10
+  },
+  headerTitle: {
+    flex: 5,
+  },
+  appName: {
+    fontFamily: "Roboto_900Black",
+    fontSize: 25,
+    marginLeft: '12%',
+    marginTop: 18,
+    color: "#522289",
   },
   iconNotification: {
+
     borderRadius: Math.round(Dimensions.get('window').width + Dimensions.get('window').height) / 2,
     width: 40,
     height: 40,
     backgroundColor: '#fff',
     marginTop: 18,
-    marginLeft: 26
+    marginLeft: '5%',
+    backgroundColor: '#fff'
   },
-  signout: {
-    borderRadius: Math.round(Dimensions.get('window').width + Dimensions.get('window').height) / 2,
-    width: 40,
-    height: 40,
-    backgroundColor: '#fff',
-    marginTop: 18,
-    marginLeft: 20
+  icon: {
+    flex: 1,
   },
   iconNoti: {
     borderRadius: Math.round(Dimensions.get('window').width + Dimensions.get('window').height) / 2,
     width: 40,
     height: 40,
   },
-  iconSignout: {
-    // borderRadius: Math.round(Dimensions.get('window').width + Dimensions.get('window').height) / 2,
-    width: 27,
-    height: 27,
-    marginTop: 6,
-    marginLeft: 8
-  },
   map: {
-    marginTop: 9,
-    position: 'absolute',
+    marginVertical: 5,
     marginHorizontal: 10,
+    flex: 5.5,
     width: 320,
     height: 380,
     borderRadius: 30
 
   },
   viewMap: {
+    flex: 4,
     alignItems: 'center',
     justifyContent: 'center',
-    overflow: 'hidden',
-    marginTop: 40,
     marginHorizontal: 10,
-    width: 340,
-    height: 400,
     borderRadius: 10,
-    backgroundColor: '#c4def6'
   }
   ,
   rate: {
-    position: 'absolute',
-    marginTop: 540,
-    marginLeft: 20
+    flex: 1,
+    width: '100%',
+    marginHorizontal: 10,
+    marginTop: 10
   },
-  starList:{
+  starList: {
     flexDirection: 'row',
-    width: 200,
-    height:40
+    width: '100%',
+    height: 40
   },
-  iconStar:{
-    marginHorizontal : 5,
+  iconStar: {
+    marginHorizontal: 5,
     width: 30,
-    height:30
+    height: 30
   },
   text: {
     color: "#000",
@@ -90,22 +86,22 @@ const styles = StyleSheet.create({
     fontFamily: "Roboto_500Medium",
   },
   status: {
-    position: 'absolute',
-    marginTop: 605,
-    marginLeft: 20
+    flex: 1,
+    width:'100%'
 
   },
-  statusText:{
-    alignSelf:'center',
-    marginTop: 100,
-    marginVertical: 5,
+  statusText: {
+    flex: 1,
+    alignSelf: 'center',
+    marginVertical: 8,
     color: "#000",
     fontSize: 17,
     fontFamily: "Roboto_500Medium",
   },
   preference: {
-    marginLeft:150
-   
+    flex: 0.5,
+   alignSelf:'center'
+
   }
 });
 
