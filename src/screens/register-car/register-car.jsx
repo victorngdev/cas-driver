995fb86bfe319428f7f186cade64bb07a2421051
Phung <nguyenphungse@gmail.com>
2020-10-17 13:29:21 +0700

    RegisterCar - Edit Screen when type input keyboard

--- src/screens/register-car/register-car.jsx
+++ src/screens/register-car/register-car.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { StyleSheet, Text, View, TextInput, KeyboardAvoidingView, Platform, Keyboard } from 'react-native';
 import BackgroundImage from '../../components/background-screen.component';
 import ButtonText from '../../components/button-text.component';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
@@ -9,74 +9,93 @@ import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 function RegisterCarScreen(props) {
   return (
     <View style={styles.container}>
+
       <BackgroundImage>
+
         <View style={{
           flexDirection: 'row',
           marginTop: 40,
           alignItems: 'center',
           paddingHorizontal: 10
         }}>
-          <Icon name='menu' size={30} color='#a2a2db'   style={{ width: 20 }} onPress={() => props.navigation.openDrawer()} />
-          </View>
-        <View style={styles.header}>
-          <Text style={styles.headerTitle}>Đăng kí xe</Text>
-          <Text style={styles.securityText}>Thông tin cá nhân sẽ được bảo mật theo chính sách, quy định của nhà nước</Text>
+          <Icon name='menu' size={30} color='#a2a2db' style={{ width: 20 }} onPress={() => props.navigation.openDrawer()} />
         </View>
-        <View style={styles.registerTextInfo}>
-          <View style={styles.name}>
-            <Text style={styles.titleInfo}>Họ và tên</Text>
-            <TextInput style={styles.titleInput} placeholder='' />
+
+        <View style={styles.contain}>
+
+          <View style={styles.header}>
+            <Text style={styles.headerTitle}>Đăng kí xe</Text>
+            <Text style={styles.securityText}>Thông tin cá nhân sẽ được bảo mật theo chính sách, quy định của nhà nước</Text>
           </View>
-          <View style={styles.phone}>
-            <Text style={styles.titleInfo}>Số điện thoại</Text>
-            <TextInput style={styles.titleInput} keyboardType='numeric' placeholder='' />
-          </View>
-          <View style={styles.licensePlate}>
-            <Text style={styles.titleInfo}>Biển số xe</Text>
-            <TextInput style={styles.titleInput} placeholder='' />
+
+          <View style={styles.registerTextInfo}>
+            <KeyboardAvoidingView behavior={Platform.OS == 'android' ? 'padding' : null} >
+              <View style={styles.name}>
+                <Text style={styles.titleInfo}>Họ và tên</Text>
+                <TextInput style={styles.titleInput} placeholder='Lê Quang Huy' onPress={() => {
+                  toggleAction();
+                }} />
+              </View>
+              <View style={styles.phone}>
+                <Text style={styles.titleInfo}>Số điện thoại</Text>
+                <TextInput style={styles.titleInput} keyboardType='numeric' placeholder='0905222007' onPress={() => {
+                  toggleAction();
+                }} />
+              </View>
+              <View style={styles.licensePlate}>
+                <Text style={styles.titleInfo}>Biển số xe</Text>
+                <TextInput style={styles.titleInput} placeholder='' onPress={() => {
+                  toggleAction();
+                }} />
+              </View>
+              <ButtonText textContent='Tiếp tục' styleText={styles.text} onPress={() => { Keyboard.dismiss }}
+                styleButton={styles.button} gotoScreen={() => props.navigation.navigate("RegisterCarImage")} />
+
+            </KeyboardAvoidingView>
           </View>
         </View>
 
-        <ButtonText textContent='Tiếp tục' styleText={styles.text}
-          styleButton={styles.button} gotoScreen={() => props.navigation.navigate("RegisterCarImage")} />
 
       </BackgroundImage>
 
 
-    </View>
+    </View >
   )
 }
 
 const styles = StyleSheet.create({
   container: {
-
     flex: 1,
     flexDirection: "column",
 
   },
+  contain: {
+    flex: 1,
+    flexDirection: "column",
+  },
   header: {
-    marginTop: 30,
-    alignContent: 'center',
+    flex: 1,
   },
   headerTitle: {
     textAlign: "center",
+    marginTop: 10,
     color: '#000',
     // fontWeight: 'bold',
     fontSize: 30
   },
   securityText: {
-    marginTop: 10,
     marginHorizontal: 8,
     fontSize: 17,
     textAlign: 'center',
     color: 'gray'
   },
   registerTextInfo: {
-    marginTop: 25,
+    flex: 3,
     marginHorizontal: 10,
+    marginTop:5
   },
   titleInfo: {
-    marginTop: 15,
+    marginTop: 5,
     fontSize: 18,
     color: '#000'
   },
@@ -89,7 +108,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#D4EAF6'
   },
   button: {
-    marginTop: 30,
+    marginTop: 5,
     backgroundColor: "#FFAB2E",
     borderRadius: 15,
     width: 100,
